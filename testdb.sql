d61a600f130b6f43e2feb56fbafa7dde89cba993
motar <motaruc@gmail.com>
2026-02-05 18:54:02 +0800

    last last

--- testdb.sql
+++ testdb.sql
@@ -1,87 +1,42 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 19, 2023 at 04:22 AM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 5.6.37
+-- Database: `test`
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `test`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `is_admin` int(1) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `is_admin` int(1) NOT NULL DEFAULT 0,
   `first_name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `username` varchar(150) NOT NULL,
-  `mobile` varchar(15) DEFAULT NULL,
   `last_name` varchar(150) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `password` varchar(150) NOT NULL,
-  `created_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+  `mobile` varchar(15) DEFAULT NULL,
+  `emp_desc` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
 
-INSERT INTO `users` (`id`, `is_admin`, `first_name`, `email`, `username`, `mobile`, `last_name`, `password`, `created_at`) VALUES
-(6, 0, 'John', 'johnny@yahoo.com', '', '67775554322', 'Doe', 'test', '2023-03-19 03:05:49.124006'),
-(7, 0, 'Lana', 'myemail@yahoo.com', '', '434343434', 'lang', '', '2023-03-19 03:06:12.634024'),
-(8, 1, 'Philip', 'fajr42@yahoo.com', 'fajr42', '343434343434', 'Ante', '12345', '2023-03-19 01:39:51.539059'),
-(9, 0, 'Clark', 'superman@gmail.com', 'sup123', '1234567888', 'Kent', '123', '2023-03-19 02:55:43.103477'),
-(12, 0, 'hernan', 'hsa@gmail.com', 'hsa42', '232323232', 'andrade', '1234', '2023-03-19 01:00:48.409821'),
-(14, 0, 'Some', 'some@gmail.com', 'someuser123', NULL, 'Sommee', '12345', '2023-03-19 01:07:08.513793'),
-(15, 0, 'Bong', 'bong@gmail.com', 'bong123', NULL, 'Andrade', 'iloveu', '2023-03-19 01:18:44.456415'),
-(16, 1, 'Love', 'mylove@gmail.com', 'mylove123', '9744554222', 'Ofmylife', 'love', '2023-03-19 01:37:52.547171'),
-(17, 0, 'Diablo', 'diablo@yahoo.com', 'diablo42', '666666', 'Augustus', '666', '2023-03-19 01:29:15.867745'),
-(18, 1, 'Shinok', 'shinok@gmail.com', 'shinok42', NULL, 'Dex', '123', '2023-03-19 02:45:24.740535'),
-(19, 0, 'Jen', 'jen@gmail.com', 'user1', NULL, 'Dy', '1234', '2023-03-19 03:07:32.005363'),
-(20, 0, 'Jenny', 'jen1@gmail.com', 'user2', NULL, 'Doe', '123', '2023-03-19 03:08:24.276360'),
-(21, 1, 'Bobo', 'vr123@yahoo.com', 'bobovr', '45454545', 'Vr', '1234', '2023-03-19 03:14:02.007098'),
-(22, 1, 'Vr', 'vradmin@yahoo.com', 'vr1', '34343434', 'games', '1234', '2023-03-19 03:13:55.696760'),
-(23, 0, 'Phil', 'phil@yahoo.com', '', '232323', 'Doe', '', '2023-03-19 03:13:47.121925');
 
---
--- Indexes for dumped tables
---
+INSERT INTO `users` (`is_admin`, `first_name`, `last_name`, `username`, `email`, `password`, `mobile`, `emp_desc`) VALUES
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+(1, 'Philip', 'Ante', 'fajr42', 'fajr42@yahoo.com', '12345', '343434343434', 'Administrator'),
+(0, 'John', 'Doe', '', 'johnny@yahoo.com', 'test', '67775554322', 'General Employee'),
+(0, 'Clark', 'Kent', 'sup123', 'superman@gmail.com', '123', '1234567888', 'Reporter'),
+(1, 'Love', 'Ofmylife', 'mylove123', 'mylove@gmail.com', 'love', '9744554222', 'Manager'),
 
---
--- AUTO_INCREMENT for dumped tables
---
+(0, 'Alice', 'Smith', 'alice_s', 'alice@example.com', 'pass123', '09123456701', 'Senior Developer'),
+(0, 'Robert', 'Downey', 'robert_d', 'robert@example.com', 'pass123', '09123456702', 'Project Manager'),
+(0, 'Charlie', 'Brown', 'charlie_b', 'charlie@example.com', 'pass123', '09123456703', 'UI/UX Designer'),
+(0, 'Diana', 'Prince', 'diana_p', 'diana@example.com', 'pass123', '09123456704', 'Quality Assurance'),
+(0, 'Edward', 'Norton', 'edward_n', 'edward@example.com', 'pass123', '09123456705', 'System Analyst'),
+(1, 'Fiona', 'Gallagher', 'fiona_g', 'fiona@example.com', 'admin456', '09123456706', 'HR Manager'),
+(0, 'George', 'Miller', 'george_m', 'george@example.com', 'pass123', '09123456707', 'Technical Support'),
+(0, 'Hannah', 'Baker', 'hannah_b', 'hannah@example.com', 'pass123', '09123456708', 'Frontend Developer'),
+(0, 'Ian', 'Somerhalder', 'ian_s', 'ian@example.com', 'pass123', '09123456709', 'Marketing Specialist');
 
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
